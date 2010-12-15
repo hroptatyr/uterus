65c0be135bbb1e4506b58f6383be645a524844d4
Sebastian Freundt <sebastian.freundt@ga-group.nl>
2010-12-15 22:12:38 +0000

    fix to fix, properly handle zone info in ariva muxer
    Signed-off-by: Sebastian Freundt <sebastian.freundt@ga-group.nl>

--- src/ariva.c
+++ src/ariva.c
@@ -714,8 +714,8 @@ void
 ariva_slab(mux_ctx_t ctx)
 {
 	/* open our timezone definition */
-	if (ctx->opts->zone != NULL) {
-		z = zif_read_inst(ctx->opts->zone);
+	if (ctx->opts->octx->zone != NULL) {
+		z = zif_read_inst(ctx->opts->octx->zone);
 	} else {
 		z = zif_read_inst(ariva_zone);
 	}

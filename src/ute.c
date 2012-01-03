b20943c594afc5ce9026f32a7ae5eb2fcac452bf
Sebastian Freundt <freundt@ga-group.nl>
2012-01-03 15:02:12 +0000

    minor, strip legacy option parser and replace by ggo driven parsing
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/ute.c
+++ src/ute.c
@@ -60,145 +60,41 @@ ute_cmd_sort(ute_opt_t UNUSED(octx), int UNUSED(argc), const char *argv[])
 	return 0;
 }
 
-static int
-ute_cmd_version(ute_opt_t UNUSED(o), int UNUSED(ac), const char *UNUSED(av[]))
-{
-	fputs("ute v", stdout);
-	fputs(UTE_VERSION, stdout);
-	fputc('\n', stdout);
-	return 0;
-}
-
-static const char usage[] = "\
-Usage: ute COMMAND ARGS\n\
-\n\
-Supported commands:\n\
-  help    This help screen\n\
-  mux     Generate an ute file from a tick source\n\
-  print   Print the contents of an ute file\n\
-  shnot   Generate top-level snapshots from ute files\n\
-  sort    Test sort the file\n\
-\n\
-Use 'ute help COMMAND' for more information \
-on a specific command\n\
-\n\
-Options common to all commands:\n\
--q, --quiet       Suppress all output.\n\
--v, --verbose     Be more talkative.\n\
--z, --zone NAME   Treat date/time strings as in time zone NAME.\n\
-\n\
--h, --help        Print this help screen.\n\
--V, --version     Print version information.\n\
-\n";
-
-static int
-ute_cmd_help(ute_opt_t UNUSED(octx), int UNUSED(argc), const char *argv[])
-{
-	if (argv == NULL || argv[1] == NULL) {
-		goto fallthrough;
-	} else if (strcmp(argv[1], "mux") == 0) {
-		fputs(ute_cmd_mux_help, stdout);
-	} else if (strcmp(argv[1], "print") == 0) {
-		fputs(ute_cmd_print_help, stdout);
-	} else {
-	fallthrough:
-		fputs(usage, stdout);
-	}
-	return 0;
-}
-
-static int
-ute_popt(ute_opt_t octx, int argc, char *argv[])
-{
-	int i = 0;
-
-	memset(octx, 0, sizeof(*octx));
-	if (argc <= 1) {
-		return -1;
-	}
-
-	/* common options */
-again:
-	for (i++; i < argc; i++) {
-		/* check options */
-		if (strcmp(argv[i], "-q") == 0 ||
-		    strcmp(argv[i], "--quiet") == 0) {
-			argv[i] = NULL;
-			octx->verbosity--;
-
-		} else if (strcmp(argv[i], "-v") == 0 ||
-			   strcmp(argv[i], "--verbose") == 0) {
-			argv[i] = NULL;
-			octx->verbosity++;
-
-		} else if (strcmp(argv[i], "-z") == 0 ||
-			   strcmp(argv[i], "--zone") == 0) {
-			argv[i] = NULL;
-			octx->zone = argv[++i];
-			argv[i] = NULL;
-
-		} else if (strcmp(argv[i], "-V") == 0 ||
-			   strcmp(argv[i], "--version") == 0) {
-			octx->cmd = ute_cmd_version;
-			break;
-
-		} else if (strcmp(argv[i], "-h") == 0 ||
-			   strcmp(argv[i], "--help") == 0) {
-			if (octx->cmd == NULL) {
-				/* someone just ute --help */
-				octx->cmd = ute_cmd_help;
-				break;
-			}
-			/* command already set, must be called like
-			 *   ute CMD --help
-			 * rewrite that to ute help CMD */
-			octx->argv--;
-			octx->argc++;
-			octx->cmd = ute_cmd_help;
-
-		} else if (octx->cmd == NULL) {
-			/* only break when there's no command yet */
-			break;
-		}
-	}
-
-	if (octx->cmd) {
-		return 0;
-	}
-
-	/* command checking */
-	octx->argc = argc - i;
-	octx->argv = (const char**)(argv + i);
-	if (strcmp(argv[i], "help") == 0) {
-		octx->cmd = ute_cmd_help;
-
-	} else if (strcmp(argv[i], "version") == 0) {
-		octx->cmd = ute_cmd_version;
-
-	} else if (strcmp(argv[i], "mux") == 0) {
-		octx->cmd = ute_cmd_mux_args;
-
-	} else if (strcmp(argv[i], "print") == 0) {
-		octx->cmd = ute_cmd_print_args;
-
-	} else if (strcmp(argv[i], "sort") == 0) {
-		octx->cmd = ute_cmd_sort;
-	}
-
-	goto again;
-}
+
+#if defined __INTEL_COMPILER
+# pragma warning (disable:593)
+# pragma warning (disable:181)
+#endif	/* __INTEL_COMPILER */
+#include "ute-clo.h"
+#include "ute-clo.c"
+#if defined __INTEL_COMPILER
+# pragma warning (default:593)
+# pragma warning (default:181)
+#endif	/* __INTEL_COMPILER */
 
 int
 main(int argc, char *argv[])
 {
+	struct gengetopt_args_info argi[1];
+	int res = 0;
 	struct ute_opt_s octx[1];
 
+	if (cmdline_parser(argc, argv, argi)) {
+		res = 1;
+		goto out;
+	}
+
+#if 0
 	if (ute_popt(octx, argc, argv) < 0 || octx->cmd == NULL) {
 		ute_cmd_help(octx, octx->argc, octx->argv);
 		return 1;
 	}
 
 	return octx->cmd(octx, octx->argc, octx->argv);
+#endif
+out:
+	cmdline_parser_free(argi);
+	return res;
 }
 
 /* ute.c ends here */

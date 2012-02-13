e7bb150f5c07c8259a2b3fe1a8f70bea12edd316
Sebastian Freundt <freundt@ga-group.nl>
2012-02-13 15:14:26 +0000

    cosmetics, put licence header into ute-mux.h
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/ute-mux.h
+++ src/ute-mux.h
@@ -1,5 +1,41 @@
-#if !defined INCLUDED_sumux_h_
-#define INCLUDED_sumux_h_
+/*** ute-mux.h -- muxing external sources
+ *
+ * Copyright (C) 2009 - 2012 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of uterus.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if !defined INCLUDED_ute_mux_h_
+#define INCLUDED_ute_mux_h_
 
 typedef struct mux_ctx_s *mux_ctx_t;
 typedef struct sumux_opt_s *sumux_opt_t;
@@ -42,4 +78,4 @@ extern void gesmes_slab(mux_ctx_t);
 extern void tfraw_slab(mux_ctx_t);
 extern void sl1t_mux(mux_ctx_t);
 
-#endif	/* INCLUDED_sumux_h_ */
+#endif	/* INCLUDED_ute_mux_h_ */

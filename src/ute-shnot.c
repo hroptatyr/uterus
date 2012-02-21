1d4e89fe159d49f5e6cff4fdab76d0fd90f9801f
Sebastian Freundt <freundt@ga-group.nl>
2012-02-21 08:40:08 +0000

    cosmetics, kick unused inlines
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/ute-shnot.c
+++ src/ute-shnot.c
@@ -216,7 +216,6 @@ next_stamp(shnot_ctx_t ctx, time_t ts)
 }
 
 static inline time_t
-__attribute__((unused))
 next_aligned_stamp(shnot_ctx_t ctx, time_t ts)
 {
 	return next_stamp(ctx, aligned_stamp(ctx, ts));

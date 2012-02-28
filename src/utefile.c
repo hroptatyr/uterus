780cf961f5191a8264decf8ce0b5773b4174e176
Sebastian Freundt <freundt@ga-group.nl>
2012-02-28 10:35:34 +0000

    cosmetics, polish utefile.c's file header
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/utefile.c
+++ src/utefile.c
@@ -1,10 +1,10 @@
 /*** utefile.c -- high level interface to ute files (r/w)
  *
- * Copyright (C) 2010 Sebastian Freundt
+ * Copyright (C) 2010-2012 Sebastian Freundt
  *
- * Author:  Sebastian Freundt <sebastian.freundt@ga-group.nl>
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
  *
- * This file is part of sushi/uterus.
+ * This file is part of uterus.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions

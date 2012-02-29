16d6515c2b82d0d87cf451de27a83e06e53f8224
Sebastian Freundt <freundt@ga-group.nl>
2012-02-29 08:44:33 +0000

    minor, remove unused slots `infiles' and `badfile' in sumux_opt_s
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/ute-mux.h
+++ src/ute-mux.h
@@ -56,9 +56,7 @@ struct mux_ctx_s {
 };
 
 struct sumux_opt_s {
-	const char **infiles;
 	const char *outfile;
-	const char *badfile;
 	const char *sname;
 	const char *zone;
 	/** offset for timestamps relative to something other than epoch */

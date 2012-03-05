dda727cc89e1f256f7f64562166390b8252bc0a6
Sebastian Freundt <freundt@ga-group.nl>
2012-03-05 14:27:38 +0000

    minor, actually inspect the contents and write candles of ibhist files
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/ibhist.c
+++ src/ibhist.c
@@ -71,6 +71,10 @@
 # define assert(args...)
 #endif	/* DEBUG_FLAG */
 
+#if defined __INTEL_COMPILER
+# pragma warning (disable:981)
+#endif	/* __INTEL_COMPILER */
+
 #if !defined LIKELY
 # define LIKELY(_x)	__builtin_expect((_x), 1)
 #endif
@@ -81,8 +85,20 @@
 # define UNUSED(_x)	__attribute__((unused)) _x
 #endif	/* !UNUSED */
 
-/* track the current level in the xml tree */
-static size_t depth = 0;
+struct qry_s {
+	int bar_len;
+	zif_t z;
+};
+
+struct rsp_s {
+	struct scdl_s cdl;
+	
+};
+
+struct xp_clo_s {
+	XML_Parser xp;
+	utectx_t wrr;
+};
 
 /* bummer, not the wisest global var name, fix me! */
 static zif_t z = NULL;
@@ -92,31 +108,201 @@ static zif_t z = NULL;
 static const char ibhist_zone[] = "Europe/Berlin";
 
 
+/* track the current level in the xml tree */
+static size_t depth = 0;
+static struct qry_s cur_qry = {0};
+static struct rsp_s cur_rsp = {0};
+
 static void
-el_sta(void *UNUSED(data), const char *elem, const char **attr)
+inspect_qry(utectx_t UNUSED(hdl), const char **attr)
 {
-	for (size_t i = 0; i < depth; i++) {
-		fprintf(stderr, " ");
+	for (size_t i = 0; attr[i]; i += 2) {
+		if (strcmp(attr[i], "whatToShow") == 0) {
+			uint16_t ttf = SCDL_FLAVOUR;
+			switch (attr[i + 1][0]) {
+			case 'A':
+				ttf |= SL1T_TTF_ASK;
+				break;
+			case 'B':
+				ttf |= SL1T_TTF_BID;
+				break;
+			case 'T':
+				ttf |= SL1T_TTF_TRA;
+				break;
+			default:
+				ttf = SL1T_TTF_UNK;
+				break;
+			}
+			/* write back to forming space */
+			scdl_set_ttf(&cur_rsp.cdl, ttf);
+
+		} else if (strcmp(attr[i], "formatDate") == 0) {
+			switch (attr[i + 1][0]) {
+			case '1':
+				/* berlin time */
+				cur_qry.z = z;
+				break;
+			case '2':
+				/* UTC? */
+			default:
+				cur_qry.z = NULL;
+				break;
+			}
+		} else if (strcmp(attr[i], "barSizeSetting") == 0) {
+			/* quick manual */
+			char *on;
+			long int n = strtol(attr[i + 1], &on, 10);
+			if (on == NULL || *on != ' ') {
+				goto unknown;
+			}
+			/* otherwise check the unit */
+			switch (*++on) {
+			case 'm':
+				/* should be minute */
+				cur_qry.bar_len = n * 60;
+				break;
+			case 's':
+				/* should be second */
+				cur_qry.bar_len = n;
+				break;
+			case 'h':
+				/* should be hour */
+				cur_qry.bar_len = n * 60 * 60;
+				break;
+			case 'd':
+				/* days? */
+				cur_qry.bar_len = n * 60 * 60 * 24;
+				break;
+			default:
+			unknown:
+				cur_qry.bar_len = 0;
+				break;
+			}
+		}
+		/* all other attrs are not of interest to us */
 	}
-	fprintf(stderr, "%s", elem);
+	return;
+}
+
+static void
+inspect_con(utectx_t hdl, const char **attr)
+{
+	const char *sym = NULL;
+	const char *sty = NULL;
+	const char *xch = NULL;
+	const char *exp = NULL;
+	const char *ccy = NULL;
+	const char *mul = NULL;
 
 	for (size_t i = 0; attr[i]; i += 2) {
-		fprintf(stderr, " %s= '%s'", attr[i], attr[i + 1]);
+		if (strcmp(attr[i], "symbol") == 0) {
+			sym = attr[i + 1];
+		} else if (strcmp(attr[i], "secType") == 0) {
+			sty = attr[i + 1];
+		} else if (strcmp(attr[i], "expiry") == 0) {
+			exp = attr[i + 1];
+		} else if (strcmp(attr[i], "multiplier") == 0) {
+			mul = attr[i + 1];
+		} else if (strcmp(attr[i], "exchange") == 0) {
+			xch = attr[i + 1];
+		} else if (strcmp(attr[i], "currency") == 0) {
+			ccy = attr[i + 1];
+		}
+		/* ignore everything else */
 	}
-	fprintf(stderr, "\n");
+
+	/* free what's there */
+	{
+		char tmp[32];
+		char *p = tmp;
+		uint16_t idx;
+
+		p = stpcpy(p, sym);
+		*p++ = ' ';
+		p = stpcpy(p, sty);
+		*p++ = ' ';
+		p = stpcpy(p, xch);
+		*p++ = ' ';
+		p = stpcpy(p, exp);
+		*p++ = ' ';
+		p = stpcpy(p, ccy);
+		*p++ = ' ';
+		p = stpcpy(p, mul);
+		*p = '\0';
+		/* just to speed up computing later on */
+		if ((idx = ute_sym2idx(hdl, tmp)) || idx == 0) {
+			scdl_set_tblidx(&cur_rsp.cdl, idx);
+		}
+	}
+	return;
+}
+
+static void
+inspect_row(utectx_t UNUSED(hdl), const char **attr)
+{
+/* fill is on descent, write is on ascent */
+	for (size_t i = 0; attr[i]; i += 2) {
+		const char *p = attr[i + 1];
+		if (strcmp(attr[i], "date") == 0) {
+			struct tm tm;
+			time_t sta, end;
+			strptime(p, "%Y%m%d  %H:%M:%S", &tm);
+			sta = ffff_timelocal(&tm, cur_qry.z);
+			end = sta + cur_qry.bar_len;
+			/* write back to result forming area */
+			cur_rsp.cdl.sta_ts = sta;
+			scdl_set_stmp_sec(&cur_rsp.cdl, end);
+			scdl_set_stmp_msec(&cur_rsp.cdl, 0);
+		} else if (strcmp(attr[i], "open") == 0) {
+			cur_rsp.cdl.o = ffff_m30_get_s(&p).v;
+		} else if (strcmp(attr[i], "high") == 0) {
+			cur_rsp.cdl.h = ffff_m30_get_s(&p).v;
+		} else if (strcmp(attr[i], "low") == 0) {
+			cur_rsp.cdl.l = ffff_m30_get_s(&p).v;
+		} else if (strcmp(attr[i], "close") == 0) {
+			cur_rsp.cdl.c = ffff_m30_get_s(&p).v;
+		}
+	}
+	/* no volume nor count */
+	cur_rsp.cdl.cnt = 0;
+	return;
+}
+
+static void
+el_sta(void *clo, const char *elem, const char **attr)
+{
+	struct xp_clo_s *xp_clo = clo;
+
+	if (strcmp(elem, "query") == 0) {
+		inspect_qry(xp_clo->wrr, attr);
+	} else if (strcmp(elem, "reqContract") == 0) {
+		inspect_con(xp_clo->wrr, attr);
+	} else if (strcmp(elem, "row") == 0) {
+		inspect_row(xp_clo->wrr, attr);
+	}
 	depth++;
 	return;
 }
 
 static void
-el_end(void *clo, const char *UNUSED(elem))
+el_end(void *clo, const char *elem)
 {
-	XML_Parser hdl = clo;
+	struct xp_clo_s *xp_clo = clo;
 
-	depth--;
-	if (depth == 0) {
+	if (strcmp(elem, "response") == 0) {
+		/* make sure we free up qry resources */
+		;
+	} else if (strcmp(elem, "row") == 0) {
+		/* write the candle */
+		if (cur_rsp.cdl.sta_ts > 0) {
+			ute_add_tick(xp_clo->wrr, AS_SCOM(&cur_rsp.cdl));
+			memset(&cur_rsp.cdl, 0, sizeof(cur_rsp.cdl));
+		}
+	} else if (strcmp(elem, "TWSXML") == 0) {
+		depth--;
+		assert(depth == 0);
 		/* need to reset the parser */
-		XML_StopParser(hdl, XML_TRUE);
+		XML_StopParser(xp_clo->xp, XML_TRUE);
 	}
 	return;
 }
@@ -128,10 +314,14 @@ parse(mux_ctx_t ctx)
 /* bit of a co-routine, yields lines from reader and yields parsed xml */
 	XML_Parser hdl = XML_ParserCreate(NULL);
 	expobuf_t eb = ctx->rdr;
+	struct xp_clo_s clo = {
+		.xp = hdl,
+		.wrr = ctx->wrr,
+	};
 
 reset:
 	XML_SetElementHandler(hdl, el_sta, el_end);
-	XML_SetUserData(hdl, hdl);
+	XML_SetUserData(hdl, &clo);
 
 	for (size_t carry = 0; eb_fetch_lines(eb); eb_unfetch_lines(eb)) {
 		enum XML_Status res;

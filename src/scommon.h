0382a7285716dc8b0a70599688defdfa276f5d39
Sebastian Freundt <freundt@ga-group.nl>
2012-03-14 07:39:32 +0000

    cosmetics, extend and update copyright info in scommon.h
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/scommon.h
+++ src/scommon.h
@@ -1,10 +1,10 @@
 /*** scommon.h -- common definitions for sparse storage
  *
- * Copyright (C) 2009 Sebastian Freundt
+ * Copyright (C) 2009-2012 Sebastian Freundt
  *
- * Author:  Sebastian Freundt <sebastian.freundt@ga-group.nl>
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
  *
- * This file is part of sushi.
+ * This file is part of uterus.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions

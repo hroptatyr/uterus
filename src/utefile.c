b133f06b1a8d1704e5d1a6ecf9c3a6b24639e527
Sebastian Freundt <hroptatyr@fresse.org>
2012-03-18 14:05:36 +0000

    debug, add assertion about the seek (byte) size in contrast with the number of ticks in the seek
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

--- src/utefile.c
+++ src/utefile.c
@@ -276,6 +276,7 @@ seek_page(uteseek_t sk, utectx_t ctx, uint32_t pg)
 	sk->fl = 0;
 
 	/* check if there's lone naughts at the end of the page */
+	assert(sk->szrw / sizeof(*sk->sp) > 0);
 	for (sp = sk->sp + sk->szrw / sizeof(*sk->sp);
 	     !AS_SCOM(sp - 1)->u && scom_tick_size(AS_SCOM(sp - 2)) == 1;
 	     sp--, nt++);

f4f54273b2af9b498c294ca378a9ccdc09272390
Sebastian Freundt <hroptatyr@fresse.org>
2012-03-18 14:07:11 +0000

    debug, add assertion about randomised ticks in utesort.c
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

--- src/utesort.c
+++ src/utesort.c
@@ -263,6 +263,7 @@ load_runs(uteseek_t sks, utectx_t ctx, sidx_t sta, sidx_t end, size_t npg)
 
 			/* the seeker should not give us trailing naughts */
 			assert(t->u);
+			assert((t->ttf & 0x30U) != 0x30U);
 			assert(thresh <= t->u);
 			thresh = t->u;
 			tsz = scom_tick_size(t);

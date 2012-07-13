964a60514d1efe5a4498edc3489475bc77f5626d
Sebastian Freundt <freundt@ga-group.nl>
2012-07-13 07:54:45 +0000

    cosmetics, mute compiler warnings about use of uninitialised vars
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>
    
    Conflicts:
    
    	src/tzraw.c

--- src/tzraw.c
+++ src/tzraw.c
@@ -374,6 +374,11 @@ __offs(zif_t z, int32_t t)
 		max = z->cache.trno;
 		this = max - 1;
 		min = 0;
+	} else {
+		/* we shouldn't end up here at all */
+		this = 0;
+		min = 0;
+		max = 0;
 	}
 	return (z->cache = __find_zrng(z, t, this, min, max)).offs;
 }

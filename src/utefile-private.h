08ce17a293e9da45421fd962c2cde702c270275b
Sebastian Freundt <freundt@ga-group.nl>
2012-12-03 13:52:27 +0000

    minor, provide ute_decode() in libuterus
    The first call also sets up a static lzma context (if lzma support is configured) that
    will be reused throughout successive calls to ute_decode().
    To clear up everything call ute_decode() with a NULL input buffer.
    
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/utefile-private.h
+++ src/utefile-private.h
@@ -105,6 +105,10 @@ extern void ute_add_ticks(utectx_t ctx, const void *p, size_t nticks);
  * Compress (whatever that means) BSZ bytes in BUF. */
 extern ssize_t ute_encode(void **tgt, const void *buf, const size_t bsz);
 
+/**
+ * Decompress (whatever that means) BSZ bytes in BUF. */
+extern ssize_t ute_decode(void **tgt, const void *buf, const size_t bsz);
+
 
 /* inlines */
 static inline size_t

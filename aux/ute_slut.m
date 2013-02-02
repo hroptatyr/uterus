f91312501f1d405f5b08577c1816b6ba57760536
Sebastian Freundt <freundt@ga-group.nl>
2013-02-02 10:50:04 +0000

    minor, provide ute_slut mex function mimicking ute-slut(1)
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ aux/ute_slut.m
@@ -0,0 +1,14 @@
+% ute_slut  return the symbol look-up table of an ute file
+%
+% Syntax:
+%   slut = ute_sluts(HANDLE);
+%
+% Input Arguments:
+%   HANDLE    - as previously obtained by ute_open
+%
+% Output Arguments:
+%   SLUT      - the symbols as cell array
+%
+% Copyright (C) 2013  Sebastian Freundt <freundt@ga-group.nl>
+%
+% This file is part of uterus

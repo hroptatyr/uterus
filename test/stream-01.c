9ea3a4d53f9505d64385d112c5527365ccbe1c7c
Sebastian Freundt <freundt@ga-group.nl>
2014-02-07 10:25:37 +0000

    test, use antique c89 initialisers for the streaming test
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- test/stream-01.c
+++ test/stream-01.c
@@ -12,26 +12,22 @@ main(void)
 {
 	const int ofl = UO_RDWR | UO_CREAT | UO_TRUNC | UO_STREAM;
 	utectx_t ctx = ute_open("stream-01.ute", ofl);
-	struct sl1t_s t1 = {
-		.hdr = {{
-				 .sec = 123456789U,
-				 .msec = 123,
-				 .idx = 1,
-				 .ttf = SL1T_TTF_BID,
-			 }},
-		.bid = 1234,
-		.bsz = 0,
-	};
-	struct sl1t_s t2 = {
-		.hdr = {{
-				 .sec = 123456789U,
-				 .msec = 123,
-				 .idx = 1,
-				 .ttf = SL1T_TTF_ASK,
-			 }},
-		.bid = 1236,
-		.bsz = 0,
-	};
+	struct sl1t_s t1;
+	struct sl1t_s t2;
+
+	t1.hdr->sec = 123456789U;
+	t1.hdr->msec = 123;
+	t1.hdr->idx = 1;
+	t1.hdr->ttf = SL1T_TTF_BID;
+	t1.bid = 1234000;
+	t1.bsz = 0;
+
+	t2.hdr->sec = 123456789U;
+	t2.hdr->msec = 123;
+	t2.hdr->idx = 1;
+	t2.hdr->ttf = SL1T_TTF_ASK;
+	t2.ask = 1236000;
+	t2.asz = 0;
 
 	for (size_t i = 0; i < 70000; i++) {
 		ute_add_tick(ctx, AS_SCOM(&t1));

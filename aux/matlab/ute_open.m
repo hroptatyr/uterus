b7f0a505910f85e356d44f25530e97c94319e5fd
Sebastian Freundt <freundt@ga-group.nl>
2014-09-03 10:48:42 +0000

    minor, support ute_open()ing of multiple files (via cell array)

--- aux/matlab/ute_open.m
+++ aux/matlab/ute_open.m
@@ -2,13 +2,16 @@
 %
 % Syntax:
 %   f = ute_open('FILENAME');
+%   f = ute_open({'FILENAME', ...});
 %
 % Input Arguments:
-%   FILENAME  - file name to open
+%   FILENAME   - file name to open
+%   {FILENAME} - cell array of file names to open
 %
 % Output Arguments:
-%   f         - handle associated with FILENAME
+%   f          - handle associated with FILENAME
+%   {f}        - cell array of handles associated with FILENAMEs
 %
-% Copyright (C) 2013  Sebastian Freundt <freundt@ga-group.nl>
+% Copyright (C) 2013-2014  Sebastian Freundt <freundt@ga-group.nl>
 %
 % This file is part of uterus

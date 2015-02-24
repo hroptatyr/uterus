22c787fb09284545324dca052feb9b6a9d19fc4e
Sebastian Freundt <freundt@ga-group.nl>
2015-02-24 12:32:00 +0000

    chore, zap to latest clitoris version

--- test/shack.c
+++ test/shack.c
@@ -1,6 +1,6 @@
 /*** shack.c -- sha check
  *
- * Copyright (C) 2013-2014 Sebastian Freundt
+ * Copyright (C) 2013-2015 Sebastian Freundt
  *
  * Author:  Sebastian Freundt <freundt@ga-group.nl>
  *
